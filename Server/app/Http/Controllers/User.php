90a3b7c6bc3e7992e700e468b5a2f5dc9a20227f
Sébastien <allimant.sebastien@gmail.com>
2026-03-24 10:23:40 +0100

    DEV - Use api ressources instead of all methode definitions

--- Server/app/Http/Controllers/User.php
+++ Server/app/Http/Controllers/User.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\User as ResourcesUser;
+use App\Models\User as ModelsUser;
 use Illuminate\Http\Request;
 
 class User extends Controller
 {
-    public function create(Request $request)
+    public function index()
     {
+        return ModelsUser::all()->toResourceCollection();
+    }
 
+    public function show(ModelsUser $user)
+    {
+        return new ResourcesUser($user);
+    }
+
+    public function store(Request $request) {}
+
+    public function update(Request $request, User $user) {}
+
+    public function destroy(ModelsUser $user)
+    {
+        $user->delete();
+
+        return response()->noContent();
     }
 }
--- Server/routes/api.php
+++ Server/routes/api.php
@@ -11,12 +11,5 @@ Route::middleware('jwt.auth')->group(function () {
     Route::get('me', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
 
-    Route::get('/users', function () {
-        return ModelsUser::all()->toResourceCollection();
-    });
-    Route::post('/users', [ControllersUser::class, "create"]);
-
-    Route::get('/user/{id}', function (string $id) {
-        return new User(ModelsUser::findOrFail($id));
-    });
+    Route::apiResource("users", ControllersUser::class);
 });
